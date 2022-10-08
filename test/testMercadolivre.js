83de7c1e2d5fb344f5dd1544ab8d9d311ec4c6a5
Flávio Henrique Pessini <flavio.pessini@outlook.com>
2022-10-08 11:15:58 -0300

    test da busca no mercadolivre

--- test/testMercadolivre.js
+++ test/testMercadolivre.js
@@ -1,13 +1,38 @@
 import fetch from 'node-fetch'
+import fs from 'fs'
 
 async function request() {
-    const url = 'http://localhost:8080/'
-
-    const data = await fetch(url).then((data) => data)
-
+    const serverUrl = 'http://localhost:8080/'
+    const productUrl = 'https://www.mercadolivre.com.br/'
+    const body = {
+        loja: 'Mercadolivre',
+        url: productUrl,
+        search: 'ssd 1 tb samsung evo plus',
+    }
+    const data = await fetch(serverUrl, {
+        method: 'post',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(body),
+    }).then((data) => data)
     const response = await data.text()
-
     console.log(response)
+
+    const jsonObj = JSON.parse(response)
+    fs.writeFileSync(
+        'output.json',
+        JSON.stringify(jsonObj),
+        {
+            encoding: 'utf8',
+        },
+        (err) => {
+            if (err) {
+                console.error(
+                    'An error occured while writing JSON Object to File'
+                )
+                return console.error(err)
+            }
+        }
+    )
 }
 
 request()
